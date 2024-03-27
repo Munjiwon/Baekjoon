af58351ad3ce862652d857c1f194fb295720e676
Jiwon Mun <65270986+Munjiwon@users.noreply.github.com>
2024-03-28 00:40:23 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    printf("|\\_/|\n");
+    printf("|q p|   /}\n");
+    printf("( %d )\"\"\"\\\n",0);
+    printf("|\"^\"\`    |\n");
+    printf("||_/=\\\\__|\n");
+    return 0;
+}

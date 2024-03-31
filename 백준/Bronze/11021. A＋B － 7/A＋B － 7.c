c95f01b39660f07645e2a5d0a719704792583389
Jiwon Mun <65270986+Munjiwon@users.noreply.github.com>
2024-03-31 20:03:53 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int x,a,b;
+    scanf("%d",&x);
+    for(int i=1;i<=x;i++){
+        scanf("%d %d",&a,&b);
+        printf("Case #%d: %d\n",i,a+b);
+    }
+    return 0;
+}

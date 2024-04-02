f5abfb7ab591bbebb5ed97685a0bb9740e7d90af
Jiwon Mun <65270986+Munjiwon@users.noreply.github.com>
2024-04-02 22:35:40 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+  int a[9];
+  int max=-1,num;
+  for(int i=0;i<9;i++){
+    scanf("%d",&a[i]);
+  }
+  for(int j=0;j<9;j++){
+    if(max<a[j]){
+      max=a[j];
+      num=j+1;
+    }
+  }
+  printf("%d\n%d",max,num);
+}
